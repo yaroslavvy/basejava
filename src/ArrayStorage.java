91e35b72bdcf1f14d4f135a9da3cd9be586fb00c
Vyskribentsev Yaroslav <vyskribentsev.yaroslav@gmail.com>
2022-05-29 00:32:37 +0300

    hw1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,58 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int arraySize = 0;
 
     void clear() {
+        for (int i = 0; i < arraySize; i++) {
+            storage[i] = null;
+        }
+        arraySize = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        if (arraySize < 10000) {
+            storage[arraySize] = resume;
+            ++arraySize;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < arraySize; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                resume = storage[i];
+                break;
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < arraySize; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int y = i; y < arraySize - 1; y++) {
+                    storage[y] = storage[y + 1];
+                }
+                storage[arraySize - 1] = null;
+                --arraySize;
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, arraySize);
     }
 
     int size() {
-        return 0;
+        return arraySize;
     }
 }
