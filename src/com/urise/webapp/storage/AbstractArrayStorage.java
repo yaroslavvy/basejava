5a2d805d09b1ec697f02dc05ac2cbfd839c8507b
Vyskribentsev Yaroslav <vyskribentsev.yaroslav@gmail.com>
2022-07-10 14:31:53 +0300

    hw4 ref: AbstractArrayStorage

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -8,7 +8,7 @@ import com.urise.webapp.model.Resume;
 import java.util.Arrays;
 
 public abstract class AbstractArrayStorage implements Storage {
-    public static final int STORAGE_LIMIT = 10000;
+    protected static final int STORAGE_LIMIT = 10000;
     protected final Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
