9403e817e5a919c4c9f144e2cb13a6a448bfaf91
Vyskribentsev Yaroslav <vyskribentsev.yaroslav@gmail.com>
2022-11-03 23:51:15 +0300

    hw12 ref after review by Yevhen Mentor:     1) ref StreamAPITasks;

--- src/com/urise/webapp/StreamAPITasks.java
+++ src/com/urise/webapp/StreamAPITasks.java
@@ -5,8 +5,23 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StreamAPITask2 {
+public class StreamAPITasks {
+
     public static void main(String[] args) {
+
+        //Test task №1
+        System.out.println("Test task №1");
+        int[] array1 = {1, 2, 3, 3, 2, 3};
+        System.out.println("Array1: " + Arrays.toString(array1));
+        System.out.println("minValue return: " + minValue(array1));
+
+        int[] array2 = {9, 8};
+        System.out.println("Array2: " + Arrays.toString(array2));
+        System.out.println("minValue return: " + minValue(array2));
+
+        //Test task №2
+        System.out.println();
+        System.out.println("Test task №2");
         List<Integer> oddSumIntegerList = new ArrayList<>(Arrays.asList(new Integer[]{1, 2, 3, 4, 13}));
         System.out.println("Odd sum list: " + oddSumIntegerList);
         System.out.println("Result list: " + oddOrEven(oddSumIntegerList));
@@ -14,6 +29,13 @@ public class StreamAPITask2 {
         List<Integer> evenSumIntegerList = new ArrayList<>(Arrays.asList(new Integer[]{1, 2, 3, 4, 17, 13}));
         System.out.println("Even sum list: " + evenSumIntegerList);
         System.out.println("Result list: " + oddOrEven(evenSumIntegerList));
+    }
+
+    private static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (a,b) -> 10 * a + b);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
